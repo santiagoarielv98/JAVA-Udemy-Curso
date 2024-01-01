74dc602be2ae860b5741dced6f0e0bef9d86c318
Santiago Ariel <santiagoarielv98@gmail.com>
2024-01-01 20:11:00 -0300

    Nuevos ejercicios resueltos

---
+++ src/Ejercicio2/DetalleDeFactura.java
@@ -0,0 +1,30 @@
+package Ejercicio2;
+
+import java.util.Scanner;
+
+public class DetalleDeFactura {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre de la factura:");
+        String nombreFactura = scanner.nextLine();
+
+        System.out.println("Ingrese el precio del primer producto:");
+        double precio1 = scanner.nextDouble();
+
+        System.out.println("Ingrese el precio del segundo producto:");
+        double precio2 = scanner.nextDouble();
+
+        double totalBruto = precio1 + precio2;
+        double impuesto = totalBruto * 0.19;
+        double totalNeto = totalBruto + impuesto;
+
+        String mensaje = "La factura " + nombreFactura + " tiene un total bruto de " + totalBruto + ", con un " +
+                "impuesto de " + impuesto + " y el monto después de impuesto es de " + totalNeto;
+
+        System.out.println(mensaje);
+
+
+
+    }
+}
---
+++ src/Ejercicio3/ProgramaManejoDeNombres.java
@@ -0,0 +1,17 @@
+package Ejercicio3;
+
+public class ProgramaManejoDeNombres {
+    public static void main(String[] args) {
+        String nombre1 = args[0];
+        String nombre2 = args[1];
+        String nombre3 = args[2];
+
+        String nombre1Modificado = nombre1.substring(1,2).toUpperCase() + "." + nombre1.substring(nombre1.length()-2);
+        String nombre2Modificado =
+                nombre2.substring(1,2).toUpperCase().concat(".").concat(nombre2.substring(nombre2.length()-2));
+        String nombre3Modificado = nombre3.substring(1,2).toUpperCase() + "." + nombre3.substring(nombre3.length()-2);
+
+        System.out.println(nombre1Modificado + "_" + nombre2Modificado + "_" + nombre3Modificado);
+    }
+
+}
