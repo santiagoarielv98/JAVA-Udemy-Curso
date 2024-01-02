d5d58d4ee650e6556721c328f4772ff15fbb25c7
Santiago Ariel <santiagoarielv98@gmail.com>
2024-01-02 00:57:29 -0300

    Nuevos ejercicios resueltos

---
+++ src/Ejercicio5.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        double numero = Double.parseDouble(JOptionPane.showInputDialog("Introduce la medida actual de gasolina en " +
+                "litros:"));
+
+        String mensaje = numero >= 70 ? "Estanque lleno" : numero >= 60 ? "Estanque casi lleno" : numero >= 40 ?
+                "Estanque 3/4" : numero >= 35 ? "Medio Estanque" : numero >= 20 ? "Suficiente" :
+                numero >= 1 ? "Insuficiente" : "Estanque vació";
+
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+}
---
+++ src/Ejercicio6.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        String[] nombres = new String[3];
+
+        for (int i = 0; i < 3; i++) {
+            nombres[i] = JOptionPane.showInputDialog("Ingrese el nombre y apellido de la persona " + (i + 1));
+        }
+
+        String nombreMasLargo = nombres[0];
+
+        for (int i = 1; i < 3; i++) {
+            if (nombres[i].length() > nombreMasLargo.length()) {
+                nombreMasLargo = nombres[i];
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, nombreMasLargo + " tiene el nombre más largo.");
+
+
+    }
+}
