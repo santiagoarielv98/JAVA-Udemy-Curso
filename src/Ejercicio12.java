94917df23278aeb8c009351ffee1f76b60ec892e
Santiago Ariel <santiagoarielv98@gmail.com>
2024-01-02 21:18:55 -0300

    Ejercicio 11 12 y 13 terminados

---
+++ src/Ejercicio12.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        int radio = Integer.parseInt(JOptionPane.showInputDialog("Introduce radio de un circulo: "));
+
+        double area = Math.PI * Math.pow(radio, 2);
+
+        JOptionPane.showMessageDialog(null, "El area del circulo es: " + area);
+
+    }
+}
---
+++ src/Ejercicio13.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class Ejercicio13 {
+    public static void main(String[] args) {
+
+    }
+}
